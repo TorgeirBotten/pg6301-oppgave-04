e5646539b1d2d8ba03ff55ecaf1ecc3e71afd06c
Torgeir Botten <torgeir.botten@gmail.com>
2026-09-17 15:32:35 +0200

    create new tasks

--- src/Application.jsx
+++ src/Application.jsx
@@ -6,7 +6,14 @@ export default function Application() {
     { description: "Task 2" },
     { description: "Task 1" },
   ]);
-  return (
+    const [description, setDescription] = useState("")
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        setTasks(old=> [{description},...old])
+    }
+
+    return (
     <>
       <h1>Task Application</h1>
       <ul>
@@ -14,6 +21,15 @@ export default function Application() {
           <li>a task: {t.description}</li>
         ))}
       </ul>
+        <h2>Add a new task</h2>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input type="text" value={description} onChange={e=> setDescription(e.target.value)}/>
+            </div>
+            <div>
+                <button>Add task {description}</button>
+            </div>
+        </form>
     </>
   );
 }
